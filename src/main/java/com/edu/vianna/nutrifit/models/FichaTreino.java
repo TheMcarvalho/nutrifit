11be2a069367b89d83e5f278ea801455ca25b472
Matheus <mcarvalh0@yahoo.com>
2026-06-05 12:36:22 -0300

    feat: adiciona entidade FichaTreino e relação com Cliente

---
+++ src/main/java/com/edu/vianna/nutrifit/models/FichaTreino.java
@@ -0,0 +1,27 @@
+package com.edu.vianna.nutrifit.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class FichaTreino {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private LocalDate dataInicio;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+}
