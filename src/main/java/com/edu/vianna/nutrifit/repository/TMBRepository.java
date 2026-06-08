a67ae83cb02ef418c0506b47d760c0c8b5562a1a
Matheus <mcarvalh0@yahoo.com>
2026-06-08 19:37:04 -0300

    feat: criação do repositório de meta

---
+++ src/main/java/com/edu/vianna/nutrifit/repository/TMBRepository.java
@@ -0,0 +1,12 @@
+package com.edu.vianna.nutrifit.repository;
+
+import com.edu.vianna.nutrifit.models.Cliente;
+import com.edu.vianna.nutrifit.models.TMB;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TMBRepository extends JpaRepository<TMB, Long> {
+    List<TMB> findByCliente (Cliente cliente);
+    TMB findTopByClienteOrderByDataDesc(Cliente cliente);
+}
