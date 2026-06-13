e768e990a1c5301e23e8679cf6a31a4421408c11
Matheus <mcarvalh0@yahoo.com>
2026-06-13 09:49:03 -0300

    feat: criação da classe FichaTreinoService

---
+++ src/main/java/com/edu/vianna/nutrifit/service/FichaTreinoService.java
@@ -0,0 +1,35 @@
+package com.edu.vianna.nutrifit.service;
+
+import com.edu.vianna.nutrifit.models.Cliente;
+import com.edu.vianna.nutrifit.models.FichaTreino;
+import com.edu.vianna.nutrifit.repository.FichaTreinoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FichaTreinoService {
+    @Autowired
+    FichaTreinoRepository fichaTreinoRepo;
+    public FichaTreino getFichaPorNome(String nome){
+        return fichaTreinoRepo.findByNome(nome);
+    }
+
+    public Optional<FichaTreino> getFichaPorId(Long id){
+        return fichaTreinoRepo.findById(id);
+    }
+
+    public List<FichaTreino> getTodasAsFichasCliente(Cliente cliente){
+        return fichaTreinoRepo.findByCliente(cliente);
+    }
+
+    public FichaTreino salvarFichaTreino(FichaTreino fichaTreino){
+        return  fichaTreinoRepo.save(fichaTreino);
+    }
+
+    public void deletarFichaTreino(Long id){
+        fichaTreinoRepo.deleteById(id);
+    }
+}
